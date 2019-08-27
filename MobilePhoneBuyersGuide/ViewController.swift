133d4be9dfab79ca0a9b68be7bcd463566b60d61
Patchariya Piyaaromrat <patchariya@Patchariyas-MacBook-Pro.local>
2019-08-27 11:00:15 +0700

    first storyboard

---
+++ MobilePhoneBuyersGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobilePhoneBuyersGuide
+//
+//  Created by Patchariya Piyaaromrat on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
